f1b9ac29d7a7dfe541af5d68a71c1c3afed41f83
kshah <kshah@10.10.122.151>
2018-09-20 08:19:05 -0400

    initial commit

---
+++ Shah,Kanay/src/artInProcessing/Art.java
@@ -0,0 +1,23 @@
+package artInProcessing;
+
+import processing.core.PApplet;
+
+public class Art extends PApplet
+{
+	public void setup()
+	{
+		
+	}
+	
+	public void settings()
+	{
+		
+	}
+	
+	public void draw()
+	{
+		rect(17,25,33,33);
+	}
+}
+	
+	
